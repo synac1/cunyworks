4cd788247a535fe1af6139138d78e26496773ee0
ateebkhawaja <ateebtahir@gmail.com>
2017-03-21 13:36:10 -0400

    Tests2

--- cunyworks/src/test/java/com/status418/cunyworks/dao/test/SubjectDAOImplTest.java
+++ cunyworks/src/test/java/com/status418/cunyworks/dao/test/SubjectDAOImplTest.java
@@ -1,7 +1,6 @@
 package com.status418.cunyworks.dao.test;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.junit.After;
 import org.junit.Before;
@@ -13,33 +12,30 @@ import com.status418.cunyworks.dao.SubjectDAOImpl;
 import com.status418.cunyworks.hibernate.HibernateUtil;
 
 public class SubjectDAOImplTest {
-	private SessionFactory sf = HibernateUtil.getSessionFactory();
 
-	private Session session = sf.openSession();
+	private Session session;
 
 	@Before
 	public void setUp() throws Exception {
+		session = HibernateUtil.getSessionFactory().openSession();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		session.close();
 	}
 
 	@Test
 	public void testGetAllSubjects() {
-
 		SubjectDAOImpl sb = new SubjectDAOImpl(session);
-
 		System.out.println(sb.getAllSubjects().size());
 	}
 
 	@Test
 	public void testGetAllTextBooksBySubject() {
-
 		SubjectBean sb = new SubjectBean();
 		sb.setSubjectId(1);
 		sb.setSubjectName("ENGLISH");
-
 		System.out.println(new SubjectDAOImpl(session).getAllTextBooksBySubject("MATH"));
 	}
 
@@ -56,34 +52,23 @@ public class SubjectDAOImplTest {
 	}
 
 	public void testTextBookInsert() {
-
 		Transaction tx = session.beginTransaction();
-
 		TextBookBean book = new TextBookBean();
 		book.setISBN(14);
 		book.setTextBookId(3);
 		book.setTextBookName("INTRO TO MATH");
-
 		session.save(book);
-
 		tx.commit();
-
 	}
 
+	@Test
 	public void testSubjectInsert() {
-
 		Transaction tx = session.beginTransaction();
-
 		SubjectBean sub = new SubjectBean();
-
 		sub.setSubjectId(3);
 		sub.setSubjectName("LATIN");
-		// sub.setCourses(null);
-		// sub.setTextBooks(null);
 		new SubjectDAOImpl(session).saveOrUpdate(sub);
-
 		tx.commit();
-
 	}
 
 }
