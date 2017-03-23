1b048b94b0ac45c948e95fddb7b0bfe95e637033
ateebkhawaja <ateebtahir@gmail.com>
2017-03-23 13:12:05 -0400

    Webpage and Other Templates

---
+++ cunyworks/src/main/java/com/status418/cunyworks/controllers/StudentController.java
@@ -0,0 +1,17 @@
+package com.status418.cunyworks.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "student")
+public class StudentController {
+
+
+	
+	
+	
+	
+	
+	
+}
