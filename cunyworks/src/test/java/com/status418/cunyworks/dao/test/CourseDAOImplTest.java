0f2579355baf30ec9239efea01d4303ecdf2deaa
ateebkhawaja <ateebtahir@gmail.com>
2017-03-24 14:33:02 -0400

    smallcchange

--- cunyworks/src/test/java/com/status418/cunyworks/dao/test/CourseDAOImplTest.java
+++ cunyworks/src/test/java/com/status418/cunyworks/dao/test/CourseDAOImplTest.java
@@ -6,13 +6,7 @@ public class CourseDAOImplTest {
 	
 	@Test
 	public void testGeAllCourses() {
-<<<<<<< HEAD
-		CourseDAOImpl dao = new CourseDAOImpl();
-		Set<CourseBean> allCourses = dao.getAll();
-		System.out.println(allCourses);
-=======
 
->>>>>>> branch 'master' of https://github.com/synac1/cunyworks.git
 	}
 
 	@Test
