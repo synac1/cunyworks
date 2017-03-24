10af8e40ccfec2f1ce7891897e29292ff8fcfafc
Hossain Yahya <hossain.yahya@outlook.com>
2017-03-24 14:50:05 -0400

    hj

--- cunyworks/src/main/java/com/status418/cunyworks/dao/CourseDAOImpl.java
+++ cunyworks/src/main/java/com/status418/cunyworks/dao/CourseDAOImpl.java
@@ -24,7 +24,7 @@ public class CourseDAOImpl implements CourseDAO {
 	@SuppressWarnings("unchecked")
 	@Transactional
 	@Override
-	public Set<CourseBean> getAll() {
+	public Set<CourseBean> geAll() {
 		Set<CourseBean> courses = new HashSet<CourseBean>();
 		courses.addAll(sessionFactory.getCurrentSession().createCriteria(CourseBean.class).list());
 		return courses;
