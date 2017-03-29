e609228307bb535eac8546a0d48ef6d9ad8823db
Owner <Owner@149.4.68.24>
2017-03-29 16:16:58 -0400

    made a quick and dirty login page. note entirley functional....yet

--- cunyworks/src/main/java/com/status418/cunyworks/controllers/LoginController.java
+++ cunyworks/src/main/java/com/status418/cunyworks/controllers/LoginController.java
@@ -4,16 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.status418.cunyworks.beans.StudentBean;
+import com.status418.cunyworks.beans.TeacherBean;
+import com.status418.cunyworks.data.FacadeImpl;
+
 @Controller
 public class LoginController {
-	
-	@RequestMapping(value="/login" , method= RequestMethod.POST)
-	public String submit(){
-		
-		
-		return "student";
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String submit(String username, String password) {
+
+		FacadeImpl f = new FacadeImpl();
+
+		StudentBean s = f.getByUsername(username);
+		TeacherBean t = f.getByTeacherUsername(username);
+
+		if (s != null && s.getPassword().equals(password)) {
+
+			return "student";
+		} else if (t != null && t.getPassword().equals(password)) {
+			return "teacher";
+
+		} else {
+			return "login";
+		}
 	}
-	
-	
 
 }
